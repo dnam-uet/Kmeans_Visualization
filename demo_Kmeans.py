7fc649c867f18cdd3f2f2ef75f2eefb8ba46b82e
dnam-uet <nguyendangnam257@gmail.com>
2021-05-05 17:45:26 +0700

    Create some button

--- demo_Kmeans.py
+++ demo_Kmeans.py
@@ -1,5 +1,9 @@
 import pygame
 
+def create_button(buttonName, buttonColor):
+    font = pygame.font.SysFont('sans', 40)
+    return font.render(buttonName, True, buttonColor)
+
 pygame.init()
 
 screen = pygame.display.set_mode((1200, 700))
@@ -17,8 +21,15 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BACKGROUND_PANEL = (249, 255, 230)
 
-font = pygame.font.SysFont('sans', 40)
-TEXT_PLUS = font.render('+', True, WHITE)
+
+TEXT_PLUS = create_button('+', WHITE)
+TEXT_MINUS = create_button('-', WHITE)
+TEXT_RUN = create_button('Run', WHITE)
+TEXT_RANDOM = create_button('Random', WHITE)
+TEXT_ALGORITHM = create_button('Algorithm', WHITE)
+TEXT_RESET = create_button('Reset', WHITE)
+
+k = 0
 
 while running:
     clock.tick(FPS)
@@ -33,11 +44,30 @@ while running:
     pygame.draw.rect(screen, BLACK, (850, 50, 50, 50))
     screen.blit(TEXT_PLUS, (860, 50))
 
+    # K button -
+    pygame.draw.rect(screen, BLACK, (950, 50, 50, 50))
+    screen.blit(TEXT_MINUS, (960, 50))
+
+    # K value
+    TEXT_K = create_button('K = ' + str(k), BLACK)
+    screen.blit(TEXT_K, (1050, 50))
+
+    # Run button
+
     # End draw interface
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Change K button +
+            if 850 < mouse_x < 900 and 50 < mouse_y < 100:
+                k += 1
+            if 950 < mouse_x < 1000 and 50 < mouse_y < 100:
+                if k > 0:
+                    k -= 1
 
     # Update the full display Surface to the screen
     pygame.display.flip()
